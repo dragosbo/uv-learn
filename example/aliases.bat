4070b38b9a5465feb2afc7fda1d005f6b547aaca
Dragos Boros <dragos.boros@gmail.com>
2025-03-23 19:09:30 +0100

    added new alias for the streamlit app

--- example/aliases.bat
+++ example/aliases.bat
@@ -2,3 +2,4 @@
 doskey uvv=uv --version
 doskey uvj=uv run jupyter notebook
 doskey uvr=uv run python -m ruff check . --fix
+doskey uvs=uv run -- streamlit run .\example\app.py
--- ge.bat
+++ ge.bat
@@ -1,7 +1,7 @@
 @echo off
 uv init --python 3.12 example
 cd ./example
-uv add ruff jupyter
+uv add ruff jupyter streamlit
 uv run ruff check
 uv lock
 uv sync
@@ -12,7 +12,14 @@ echo @echo off
 echo doskey uvv=uv --version
 echo doskey uvj=uv run jupyter notebook
 echo doskey uvr=uv run python -m ruff check . --fix
+echo doskey uvs=uv run -- streamlit run .\example\app.py
+
 ) > aliases.bat
+
+(
+echo import streamlit as st
+echo st.write^("hello world from Streamlit"^)
+) > app.py
 
 call .venv/Scripts/activate
 call aliases.bat
